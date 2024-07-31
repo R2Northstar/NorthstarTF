0024f71b90ecb3776c7f5fd0e56abf403c5ef913
GeckoEidechse <40122905+GeckoEidechse@users.noreply.github.com>
2024-08-01 01:24:42 +0200

    Add section for community contributors (#83)
    This section will be populated by everyone not manually mentioned on credits page that has contributed to the project according to GitHub API
    
    Co-authored-by: Barnaby <22575741+barnabwhy@users.noreply.github.com>

---
+++ src/data/community-contributors.ts
@@ -0,0 +1,9 @@
+// Auto-generated from Python script
+
+export interface CommunityContributor {
+    url?: string;
+    icon: string;
+    name: string;
+}
+export const community_contributors: CommunityContributor[] = [
+]

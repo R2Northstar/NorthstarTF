21872b717242f42659349bde3a67e4808df3c539
GeckoEidechse <gecko.eidechse+git@pm.me>
2024-07-31 17:11:04 +0200

    Add generated TypeScript community contributor list

---
+++ src/data/community-contributors.ts
@@ -0,0 +1,309 @@
+// Auto-generated from Python script
+
+export interface CommunityContributor {
+    url?: string;
+    icon: string;
+    name: string;
+}
+export const community_contributors: CommunityContributor[] = [
+    {
+        url: "https://github.com/0neGal",
+        icon: "https://avatars.githubusercontent.com/u/33325154?v=4",
+        name: "0neGal",
+    },
+    {
+        url: "https://github.com/AdobeWallHacks",
+        icon: "https://avatars.githubusercontent.com/u/78327970?v=4&s=128",
+        name: "AdobeWallHacks",
+    },
+    {
+        url: "https://github.com/AnActualEmerald",
+        icon: "https://avatars.githubusercontent.com/u/4367791?v=4&s=128",
+        name: "AnActualEmerald",
+    },
+    {
+        url: "https://github.com/ConnorDoesDev",
+        icon: "https://avatars.githubusercontent.com/u/69979824?v=4&s=128",
+        name: "ConnorDoesDev",
+    },
+    {
+        url: "https://github.com/Coopyy",
+        icon: "https://avatars.githubusercontent.com/u/26663693?v=4&s=128",
+        name: "Coopyy",
+    },
+    {
+        url: "https://github.com/Distion55x",
+        icon: "https://avatars.githubusercontent.com/u/47297018?v=4&s=128",
+        name: "Distion55x",
+    },
+    {
+        url: "https://github.com/EnderBoy9217",
+        icon: "https://avatars.githubusercontent.com/u/122132914?v=4&s=128",
+        name: "EnderBoy9217",
+    },
+    {
+        url: "https://github.com/FerretWithAGun",
+        icon: "https://avatars.githubusercontent.com/u/63244055?v=4&s=128",
+        name: "FerretWithAGun",
+    },
+    {
+        url: "https://github.com/Glacir",
+        icon: "https://avatars.githubusercontent.com/u/122596880?v=4&s=128",
+        name: "Glacir",
+    },
+    {
+        url: "https://github.com/HappyDOGE",
+        icon: "https://avatars.githubusercontent.com/u/28511119?v=4&s=128",
+        name: "HappyDOGE",
+    },
+    {
+        url: "https://github.com/IncPlusPlus",
+        icon: "https://avatars.githubusercontent.com/u/6992149?v=4&s=128",
+        name: "IncPlusPlus",
+    },
+    {
+        url: "https://github.com/JJRcop",
+        icon: "https://avatars.githubusercontent.com/u/5211576?v=4",
+        name: "JJRcop",
+    },
+    {
+        url: "https://github.com/JMM889901",
+        icon: "https://avatars.githubusercontent.com/u/41163714?v=4",
+        name: "JMM889901",
+    },
+    {
+        url: "https://github.com/Kaze-Kami",
+        icon: "https://avatars.githubusercontent.com/u/18683538?v=4&s=128",
+        name: "Kaze-Kami",
+    },
+    {
+        url: "https://github.com/L1ghtman2k",
+        icon: "https://avatars.githubusercontent.com/u/35672535?v=4&s=128",
+        name: "L1ghtman2k",
+    },
+    {
+        url: "https://github.com/MiloAkerman",
+        icon: "https://avatars.githubusercontent.com/u/40443620?v=4",
+        name: "MiloAkerman",
+    },
+    {
+        url: "https://github.com/MysteriousRSA",
+        icon: "https://avatars.githubusercontent.com/u/45333346?v=4&s=128",
+        name: "MysteriousRSA",
+    },
+    {
+        url: "https://github.com/Neoministein",
+        icon: "https://avatars.githubusercontent.com/u/57015772?v=4",
+        name: "Neoministein",
+    },
+    {
+        url: "https://github.com/NoCatt",
+        icon: "https://avatars.githubusercontent.com/u/86153630?v=4",
+        name: "NoCatt",
+    },
+    {
+        url: "https://github.com/Nyami11",
+        icon: "https://avatars.githubusercontent.com/u/116882110?v=4&s=128",
+        name: "Nyami11",
+    },
+    {
+        url: "https://github.com/Ombrezz",
+        icon: "https://avatars.githubusercontent.com/u/37753837?v=4&s=128",
+        name: "Ombrezz",
+    },
+    {
+        url: "https://github.com/Orpheus2401",
+        icon: "https://avatars.githubusercontent.com/u/62786393?v=4&s=128",
+        name: "Orpheus2401",
+    },
+    {
+        url: "https://github.com/PersonMeetup",
+        icon: "https://avatars.githubusercontent.com/u/42787368?v=4",
+        name: "PersonMeetup",
+    },
+    {
+        url: "https://github.com/RootCellar",
+        icon: "https://avatars.githubusercontent.com/u/26856445?v=4&s=128",
+        name: "RootCellar",
+    },
+    {
+        url: "https://github.com/SamLam140330",
+        icon: "https://avatars.githubusercontent.com/u/48656764?v=4",
+        name: "SamLam140330",
+    },
+    {
+        url: "https://github.com/ScureX",
+        icon: "https://avatars.githubusercontent.com/u/47725553?v=4",
+        name: "ScureX",
+    },
+    {
+        url: "https://github.com/SenorGeese",
+        icon: "https://avatars.githubusercontent.com/u/87151598?v=4",
+        name: "SenorGeese",
+    },
+    {
+        url: "https://github.com/Slipstreamm",
+        icon: "https://avatars.githubusercontent.com/u/77130743?v=4&s=128",
+        name: "Slipstreamm",
+    },
+    {
+        url: "https://github.com/Steveplays28",
+        icon: "https://avatars.githubusercontent.com/u/62797992?v=4&s=128",
+        name: "Steveplays28",
+    },
+    {
+        url: "https://github.com/Striketester",
+        icon: "https://avatars.githubusercontent.com/u/96035439?v=4&s=128",
+        name: "Striketester",
+    },
+    {
+        url: "https://github.com/TH3-S4LM0N",
+        icon: "https://avatars.githubusercontent.com/u/99826409?v=4",
+        name: "TH3-S4LM0N",
+    },
+    {
+        url: "https://github.com/TheLavaLump",
+        icon: "https://avatars.githubusercontent.com/u/129992171?v=4&s=128",
+        name: "TheLavaLump",
+    },
+    {
+        url: "https://github.com/USBDongleGoblin",
+        icon: "https://avatars.githubusercontent.com/u/73048023?v=4&s=128",
+        name: "USBDongleGoblin",
+    },
+    {
+        url: "https://github.com/VITALISED",
+        icon: "https://avatars.githubusercontent.com/u/39478251?v=4",
+        name: "VITALISED",
+    },
+    {
+        url: "https://github.com/VictorPLopes",
+        icon: "https://avatars.githubusercontent.com/u/77900343?v=4&s=128",
+        name: "VictorPLopes",
+    },
+    {
+        url: "https://github.com/WofWca",
+        icon: "https://avatars.githubusercontent.com/u/39462442?v=4&s=128",
+        name: "WofWca",
+    },
+    {
+        url: "https://github.com/anjannair",
+        icon: "https://avatars.githubusercontent.com/u/22571613?v=4&s=128",
+        name: "anjannair",
+    },
+    {
+        url: "https://github.com/begin-theadventure",
+        icon: "https://avatars.githubusercontent.com/u/99835765?v=4",
+        name: "begin-theadventure",
+    },
+    {
+        url: "https://github.com/connieprice",
+        icon: "https://avatars.githubusercontent.com/u/5340283?v=4&s=128",
+        name: "connieprice",
+    },
+    {
+        url: "https://github.com/cyrv6737",
+        icon: "https://avatars.githubusercontent.com/u/68307100?v=4",
+        name: "cyrv6737",
+    },
+    {
+        url: "https://github.com/fvnkhead",
+        icon: "https://avatars.githubusercontent.com/u/98965760?v=4&s=128",
+        name: "fvnkhead",
+    },
+    {
+        url: "https://github.com/gotimo2",
+        icon: "https://avatars.githubusercontent.com/u/61739017?v=4&s=128",
+        name: "gotimo2",
+    },
+    {
+        url: "https://github.com/hxdeci",
+        icon: "https://avatars.githubusercontent.com/u/58310402?v=4&s=128",
+        name: "hxdeci",
+    },
+    {
+        url: "https://github.com/itscynxx",
+        icon: "https://avatars.githubusercontent.com/u/70904206?v=4",
+        name: "itscynxx",
+    },
+    {
+        url: "https://github.com/jakubiakdev",
+        icon: "https://avatars.githubusercontent.com/u/63729559?v=4&s=128",
+        name: "jakubiakdev",
+    },
+    {
+        url: "https://github.com/jeremychone",
+        icon: "https://avatars.githubusercontent.com/u/74671?v=4&s=128",
+        name: "jeremychone",
+    },
+    {
+        url: "https://github.com/luludotdev",
+        icon: "https://avatars.githubusercontent.com/u/2358182?v=4",
+        name: "luludotdev",
+    },
+    {
+        url: "https://github.com/microraptor",
+        icon: "https://avatars.githubusercontent.com/u/15663536?v=4&s=128",
+        name: "microraptor",
+    },
+    {
+        url: "https://github.com/piebaker",
+        icon: "https://avatars.githubusercontent.com/u/73162560?v=4&s=128",
+        name: "piebaker",
+    },
+    {
+        url: "https://github.com/rrrfffrrr",
+        icon: "https://avatars.githubusercontent.com/u/11633118?v=4&s=128",
+        name: "rrrfffrrr",
+    },
+    {
+        url: "https://github.com/rwynx",
+        icon: "https://avatars.githubusercontent.com/u/101754031?v=4&s=128",
+        name: "rwynx",
+    },
+    {
+        url: "https://github.com/sneakycrow",
+        icon: "https://avatars.githubusercontent.com/u/106559028?v=4&s=128",
+        name: "sneakycrow",
+    },
+    {
+        url: "https://github.com/stygia-dev",
+        icon: "https://avatars.githubusercontent.com/u/116909003?v=4&s=128",
+        name: "stygia-dev",
+    },
+    {
+        url: "https://github.com/sussyenby",
+        icon: "https://avatars.githubusercontent.com/u/99197472?v=4&s=128",
+        name: "sussyenby",
+    },
+    {
+        url: "https://github.com/themoonisacheese",
+        icon: "https://avatars.githubusercontent.com/u/15526977?v=4&s=128",
+        name: "themoonisacheese",
+    },
+    {
+        url: "https://github.com/theroylee",
+        icon: "https://avatars.githubusercontent.com/u/55896140?v=4&s=128",
+        name: "theroylee",
+    },
+    {
+        url: "https://github.com/theunkn0wn1",
+        icon: "https://avatars.githubusercontent.com/u/3110986?v=4&s=128",
+        name: "theunkn0wn1",
+    },
+    {
+        url: "https://github.com/weblate",
+        icon: "https://avatars.githubusercontent.com/u/1607653?v=4&s=128",
+        name: "weblate",
+    },
+    {
+        url: "https://github.com/xamionex",
+        icon: "https://avatars.githubusercontent.com/u/57235791?v=4",
+        name: "xamionex",
+    },
+    {
+        url: "https://github.com/zxcPandora",
+        icon: "https://avatars.githubusercontent.com/u/81985226?v=4&s=128",
+        name: "zxcPandora",
+    },
+]

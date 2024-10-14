1b957f3e55ac7f54196f79dab83c10eeb7a33980
GeckoEidechse <gecko.eidechse+git@pm.me>
2024-10-14 14:20:41 +0200

    Increase minimum review character length to be considered for counting

--- scripts/scrape-for-reviews.py
+++ scripts/scrape-for-reviews.py
@@ -78,7 +78,7 @@ review_dict = defaultdict(list)
 
 def is_trivial_review(review_text: str):
     """Perform a variety of checks to determine whether a review should be discarded due to not being extensive enough"""
-    min_review_length = 20
+    min_review_length = 30
     if "lgtm" in review_text.lower() and len(review_text) < min_review_length:
         return True
 

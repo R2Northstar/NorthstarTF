ecc1e3f83c233509cfbc585363baee96689708df
GeckoEidechse <gecko.eidechse+git@pm.me>
2024-10-19 00:26:18 +0200

    clean up review counts should be committed from CI

--- src/data/reviewer-count-monthly.ts
+++ src/data/reviewer-count-monthly.ts
@@ -6,39 +6,4 @@ export interface ReviewCount {
     count: number;
 }
 export const review_counts_monthly: ReviewCount[] = [
-    {
-        url: "https://github.com/Alystrasz",
-        name: "Alystrasz",
-        count: 6,
-    },
-    {
-        url: "https://github.com/catornot",
-        name: "catornot",
-        count: 1,
-    },
-    {
-        url: "https://github.com/GeckoEidechse",
-        name: "GeckoEidechse",
-        count: 19,
-    },
-    {
-        url: "https://github.com/JMM889901",
-        name: "JMM889901",
-        count: 3,
-    },
-    {
-        url: "https://github.com/NachosChipeados",
-        name: "NachosChipeados",
-        count: 2,
-    },
-    {
-        url: "https://github.com/uniboi",
-        name: "uniboi",
-        count: 1,
-    },
-    {
-        url: "https://github.com/Zanieon",
-        name: "Zanieon",
-        count: 1,
-    },
 ]

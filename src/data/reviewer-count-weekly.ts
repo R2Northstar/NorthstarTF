65ae5c1ff043385d9fc8409ccd7de7a84f26c8a6
GeckoEidechse <40122905+GeckoEidechse@users.noreply.github.com>
2024-11-03 00:56:45 +0000

    Update review counts

--- src/data/reviewer-count-weekly.ts
+++ src/data/reviewer-count-weekly.ts
@@ -7,8 +7,18 @@ export interface ReviewCount {
 }
 export const review_counts_weekly: ReviewCount[] = [
     {
+        url: "https://github.com/Alystrasz",
+        name: "Alystrasz",
+        count: 3,
+    },
+    {
         url: "https://github.com/ASpoonPlaysGames",
         name: "ASpoonPlaysGames",
         count: 1,
+    },
+    {
+        url: "https://github.com/GeckoEidechse",
+        name: "GeckoEidechse",
+        count: 2,
     },
 ]
